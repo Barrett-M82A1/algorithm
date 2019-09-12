4f7ea720fc3e0725a24973ae4a0b1a7ef083e312
markus <i@yoyoyo.me>
2019-09-12 14:01:09 +0800

    优化选择排序，同时找出最大与最小值，减少2/1递归次数

--- selectSort.go
+++ selectSort.go
@@ -3,18 +3,27 @@ package main
 import "fmt"
 
 func main() {
-	data := [9]int{9, 6, 1, 3, 7, 2, 8, 4, 5}
+	data := [9]int{5, 6, 1, 3, 7, 4, 8, 2, 9}
 
-	pointer := 0
+	var max, min int
 
-	for j := 0; j <= len(data)-1; j++ {
-		for i := j + 1; i <= len(data)-2; i++ {
-			if data[i] < data[pointer] {
-				pointer = i
+	for j := 0; j <= len(data)/2-1; j++ {
+
+		max, min = j, j
+
+		for i := j; i <= len(data)-j-2; i++ {
+			if data[max] < data[i+1] {
+				max = i + 1
+			}
+
+			if data[min] > data[i+1] {
+				min = i + 1
 			}
 		}
 
-		data[j], data[pointer] = data[pointer], data[j]
+		data[j], data[min] = data[min], data[j]
+
+		data[len(data)-j-1], data[max] = data[max], data[len(data)-j-1]
 	}
 
 	fmt.Println(data)
